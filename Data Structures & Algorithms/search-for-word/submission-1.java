2fe2aac34d90352b757c90c54a438da9738d9979
Ajinkya D <138004402+AjinkyaD3@users.noreply.github.com>
2026-07-01 09:47:22 +0530

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.java
@@ -0,0 +1,55 @@
+class Solution {
+
+    public boolean exist(char[][] board, String word) {
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // Try starting from every cell
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (backtrack(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean backtrack(char[][] board, String word,
+                              int row, int col, int index) {
+
+        // Entire word has been matched
+        if (index == word.length()) {
+            return true;
+        }
+
+        // Out of bounds
+        if (row < 0 || row >= board.length ||
+            col < 0 || col >= board[0].length) {
+            return false;
+        }
+
+        // Current character doesn't match
+        if (board[row][col] != word.charAt(index)) {
+            return false;
+        }
+
+        // Save the character and mark this cell as visited
+        char temp = board[row][col];
+        board[row][col] = '#';
+
+        // Explore all 4 directions
+        boolean found =
+                backtrack(board, word, row + 1, col, index + 1) ||
+                backtrack(board, word, row - 1, col, index + 1) ||
+                backtrack(board, word, row, col + 1, index + 1) ||
+                backtrack(board, word, row, col - 1, index + 1);
+
+        // Restore the original character (backtracking)
+        board[row][col] = temp;
+
+        return found;
+    }
+}
